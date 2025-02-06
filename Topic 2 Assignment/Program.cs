c5dfcd63cd01a8a827cec1ec61bdda0fb8e7291b
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-06 14:33:52 -0500

    Completed more of the assignment.

--- Topic 2 Assignment/Program.cs
+++ Topic 2 Assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Linkin Krysa
+            Console.WriteLine("The year I was born minus my age is:" + (2008 - 16));
+            Console.WriteLine();
+            Console.WriteLine("The average of the answer below is:" + (45/3));
+            Console.WriteLine(14 + 15 + 16);
+            Console.WriteLine();
+            Console.WriteLine("The average of the answer below is:" + (62.0 / 4));
+            Console.WriteLine(14.0 + 15.0 + 16.0 + 17.0);
+            Console.WriteLine();
+
+
+
+
+            
         }
     }
 }
